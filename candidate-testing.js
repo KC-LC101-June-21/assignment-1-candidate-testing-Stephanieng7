45a261a20122bf6fe98bebe6ce07da11be29aeca
Stephanieng7 <58015082+Stephanieng7@users.noreply.github.com>
2021-06-17 23:46:13 -0500

    Update candidate-testing.js

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,11 @@ let candidateName = "" + ""
 let question = "Who was the first American woman in space? " 
 let correctAnswer = "Sally Ride"
 let candidateAnswer = ""
-let questions = [("1)Who was the first American woman in space?  "), ("2)True or false: 5 kilometer == 5000 meters?  ")  , ("3)(5+3)/2 * 10 =?  "),  ("4)Given the array[8, Orbit, Trajectory, 45] what entry is at index 2?  "), ("5)What is the minimum crew size for the ISS?  ")];
+let questions = [ '1)Who was the first American woman in space?  ', 
+'2)True or false: 5 kilometer == 5000 meters?  ', 
+'3)(5+3)/2 * 10 =?  ', 
+'4)Given the array[8, Orbit, Trajectory, 45] what entry is at index 2?  ',
+ '5)What is the minimum crew size for the ISS?  ' ];
 let correctAnswers = ["Sally Ride",  "true",  "40",  "Trajectory",  "3"];
 let candidateAnswers = [];
 
@@ -28,10 +32,11 @@ function askQuestion(){
   let total = 0
    for(let i = 0; i < questions.length; i++){
       candidateAnswers = input.question(questions[i]);
-      console.log("Your Response:" + candidateAnswers);
-      console.log("Correct Answer:" + correctAnswers[i]);
+      console.log("Your Response:" + candidateAnswers.toLowerCase());
+      console.log("Correct Answer:" + correctAnswers[i].toLowerCase());
         if(candidateAnswers === correctAnswers[i]){
               total++}
+
 } 
 let grade = (total/questions.length * 100)
   if(grade >= 80){
@@ -39,6 +44,7 @@ let grade = (total/questions.length * 100)
 }if(grade < 80){
   console.log ('>>> STATUS: FAILED <<<');
 }
+
 
 console.log(`>>> Overall Grade: ${grade} %   ${total} of 5 responses correct <<<`);
    
@@ -60,6 +66,7 @@ console.log(`>>> Overall Grade: ${grade} %   ${total} of 5 responses correct <<<
 
 
 
+  
 
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
@@ -72,7 +79,7 @@ function runProgram() {
   // TODO 1.1c: Ask for candidate's name //
   
   askQuestion();
- 
+  
 }
 
 // Don't write any code below this line //
